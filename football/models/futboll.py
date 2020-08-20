721794a618230538e4fe146762c6fdea26a08b5d
unknown <lirdibudo@yahoo.com>
2020-08-20 12:02:19 +0200

    ad one and compute functions started

--- football/models/futboll.py
+++ football/models/futboll.py
@@ -1,5 +1,6 @@
 import base64
 import logging
+from datetime import datetime
 
 from odoo import api, fields, models
 from odoo import tools, _
@@ -32,7 +33,8 @@ class Ekipi(models.Model):
     buxheti = fields.Integer()
     vlera_e_ekpit = fields.Integer(default_value=False)
     lojtaret_ids = fields.One2many(comodel_name='lojtari', inverse_name='ekipi_aktual_id', string='')
-    lojtaret_prone_ids = fields.One2many(comodel_name='lojtari', inverse_name='ekipi_nga_i_cili_ka_ardhur_id', string='')
+    lojtaret_prone_ids = fields.One2many(comodel_name='lojtari', inverse_name='ekipi_nga_i_cili_ka_ardhur_id',
+                                         string='')
 
     def llogarit_vlera_e_ekipit(self):
         for lojtar in self.lojtaret_ids:
@@ -53,13 +55,15 @@ class Ekipi(models.Model):
 class Sezon_ekip(models.Model):
     _name = "sezonekip"
 
+
     # id_ekipit = fields.Integer(required=True)
     ekipit_id = fields.Many2one(comodel_name='ekipet', string='Ekipi', required=False)
     sezonit_id = fields.Many2one(comodel_name='sezone', string='Sezonet', required=False)
     fitore = fields.Integer(required=True)
     humbje = fields.Integer(required=True)
     barazime = fields.Integer(required=True)
-    piket = fields.Integer()
+    piket = fields.Integer(compute='llogarit_piket')
+    
 
     def llogarit_piket_nga_rezultatet(self):
         ndeshje = self.env["ndeshje"].search(
@@ -80,8 +84,14 @@ class Sezon_ekip(models.Model):
                 else:
                     self.humbje += 1
 
+
+
+
+    @api.multi
+    @api.depends('fitore', 'barazime')
     def llogarit_piket(self):
-        self.piket = 3 * self.fitore + self.barazime
+        for record in self:
+            record.piket = 3 * record.fitore + record.barazime
 
 
 class Ndeshje(models.Model):
@@ -96,6 +106,7 @@ class Ndeshje(models.Model):
     ekipi_away_id = fields.Many2one(related='sezonekip_away_id.ekipit_id', store=True)
     java = fields.Integer(required=True)
 
+    @api.onchange('gola_home', 'gola_away')
     def perditeso_piket(self):
         if self.gola_home > self.gola_away:
             self.sezonekip_home_id.fitore += 1
@@ -108,35 +119,45 @@ class Ndeshje(models.Model):
             self.sezonekip_away_id.fitore
 
     # ekipi_a_id = fields.Many2one(comodel_name='ekipet')
-
+    @api.onchange('ekipi_home_id','ekipi_away_id')
+    def kontrollo_ndeshjet(self):
+        pass
 
 class Transferime(models.Model):
     _name = "transferime"
 
     # id = fields.Integer()
     id_lojtarit_id = fields.Many2one(comodel_name='lojtari', string='Transferimi', required=False)
-    ekipi_from_id = fields.Many2one(comodel_name='ekipet', string='Ekipifrom', required=False) #ekipi me te cilin luan
-    ekipi_to_id = fields.Many2one(comodel_name='ekipet', string='Ekipito', required=False) #ekipi tek i cili do te transferohet
+    ekipi_from_id = fields.Many2one(comodel_name='ekipet', string='Ekipifrom', required=False)  # ekipi me te cilin luan
+    ekipi_to_id = fields.Many2one(comodel_name='ekipet', string='Ekipito',
+                                  required=False)  # ekipi tek i cili do te transferohet
     lloji_kontrate = fields.Boolean(string="Kontrate huazimi",
                                     description="uncheck kontrate blerje, check kontrate huazimi", default=False)
     kohezgjatja_e_kontrates = fields.Date()
     data_e_nenshkrimit = fields.Date()
     vite_kontrate = fields.Float(string='Kohezgjatja ne vite', compute='llogarit_kohezgjatje')
-
-    @api.multi
-    @api.depends('data_e_nenshkrimit', 'kohezgjatja_e_kontrates')
-    def llogarit_kohezgjatje(self):
-        for record in self:
-            return True
-
     state = fields.Selection(string='Statusi', default="draft",
                              selection=[('draft', 'Draft'),
                                         ('ne_ekzekutim', 'Ne ekzekutim'),
                                         ('ekzekutuar', 'Ekzekutuar')])
 
+    @api.multi
+    @api.depends('data_e_nenshkrimit', 'kohezgjatja_e_kontrates')
+    def llogarit_kohezgjatje(self):
+        for record in self:
+            if record.kohezgjatja_e_kontrates and record.data_e_nenshkrimit:
+                kohezgjatja_e_kontrates = record.kohezgjatja_e_kontrates if type(
+                    record.kohezgjatja_e_kontrates) is not str else datetime.strptime(record.kohezgjatja_e_kontrates,
+                                                                                      '%Y-%m-%d')
+                data_e_nenshkrimit = record.data_e_nenshkrimit if type(
+                    record.data_e_nenshkrimit) is not str else datetime.strptime(record.data_e_nenshkrimit, '%Y-%m-%d')
+                dite_kontrate = (kohezgjatja_e_kontrates - data_e_nenshkrimit)
+                record.vite_kontrate = dite_kontrate.days / 365.0
+
     @api.onchange('id_lojtarit_id')
     def percakto_automatikisht_ekipin_me_te_cilin_luan(self):
-        self.ekipi_from_id =  self.id_lojtarit_id.ekipi_aktual_id.id
+        if self.id_lojtarit_id.ekipi_aktual_id.id:
+            self.ekipi_from_id = self.id_lojtarit_id.ekipi_aktual_id.id
 
     def kalo_ekzekutim(self):
         self.state = 'ne_ekzekutim'
